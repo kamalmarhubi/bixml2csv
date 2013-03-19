c5bb05bd0f23d6d675ef6556b6c0b4cf318a2b1e
Kamal Marhubi <kamal@marhubi.com>
2013-03-19 18:21:50 -0400

    remove useless unicode csv class

--- parse.py
+++ parse.py
@@ -1,47 +1,14 @@
 import xml.etree.cElementTree as etree
-import cStringIO
-import codecs
-import csv
 import sys
 import os
 import logging
 
-logger = logging.getLogger(__name__)
-rootLogger = logging.getLogger()
-rootLogger.addHandler(logging.StreamHandler())
-#rootLogger.setLevel(logging.DEBUG)
-
-
-class UnicodeWriter:
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
-
 if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    rootLogger = logging.getLogger()
+    rootLogger.addHandler(logging.StreamHandler())
+    rootLogger.setLevel(logging.INFO)
+
     for dirname, dirnames, filenames in os.walk(sys.argv[1]):
         for filename in filenames:
             fullpath = os.path.join(dirname, filename)
