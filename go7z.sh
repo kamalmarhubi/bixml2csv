75ed0b71ce06abcee697ebf251587aa81f7af903
Kamal Marhubi <kamal@marhubi.com>
2013-06-13 11:21:15 -0400

    remove useless scripts
    The dedup.py was a bad attempt at streaming deduplication; the go7z
    script was to process Fabrice's 7zipped tar of xml files.

--- go7z.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-FILE=$1
-OUTDIR=${2-csv}
-BASENAME=`basename $FILE .7z`
-DIRNAME=`dirname $FILE`
-7z e -so $DIRNAME/$BASENAME.7z \
-	| python parse.py --header -v 2> $OUTDIR/$BASENAME.stderr \
-	| xz > $OUTDIR/$BASENAME.csv.xz
